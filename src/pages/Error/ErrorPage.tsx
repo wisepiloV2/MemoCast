55dc24cffd8f8eea4e0420f244b02a8f6a77f9ff
Wisepilo <maty20022@gmail.com>
2026-08-07 20:43:23 -0300

    refactor: ErrorPage finalizando paginas para lanzar version 1.0

--- src/pages/Error/ErrorPage.tsx
+++ src/pages/Error/ErrorPage.tsx
@@ -1,4 +1,5 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { Button } from '../../component/Button/Button';
 import styles from './ErrorPage.module.css';
 import { MainLayout } from '../../component/layout/MainLayout';
 
@@ -8,15 +9,13 @@ interface ErrorPageProps {
 }
 
 export function ErrorPage({ title, subtitle }: ErrorPageProps) {
+  const navigate = useNavigate();
   return (
     <MainLayout>
       <div className={styles.container}>
         <h1 className={styles.title}>{title}</h1>
         <p className={styles.subtitle}>{subtitle}</p>
-        
-        <Link to="/" className={styles.btnHome}>
-          Go home
-        </Link>
+        <Button variant='primary' onClick={() => navigate("/")}>Go home</Button>
       </div>
     </MainLayout>
   );
