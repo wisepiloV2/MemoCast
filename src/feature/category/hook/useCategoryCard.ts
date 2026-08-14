561da86fb657de0c858720b488741543bc6e6595
Wisepilo <maty20022@gmail.com>
2026-08-13 21:59:30 -0300

    fix: arreglos para el build

--- src/feature/category/hook/useCategoryCard.ts
+++ src/feature/category/hook/useCategoryCard.ts
@@ -1,4 +1,4 @@
-import { FormEvent, useState } from 'react';
+import { type FormEvent, useState } from 'react';
 import { categoryService } from '../service/categoryService';
 
 type ModalView = 'options' | 'edit' | 'delete';
